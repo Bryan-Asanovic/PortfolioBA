d9f42bd1e35e6d56629e6b3d8daaa160a84e5708
Bryan <0346194@student.rocvantwente.nl>
2024-12-16 13:42:02 +0100

    update meta tags and project name in index.html and manifest.json; remove placeholder text from Contact component

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -8,7 +8,6 @@ function Contact() {
       <div className="items-container">
         <div className="contact_wrapper">
           <h1>Contact Me</h1>
-          <p>Got a project waiting to be realized? Let's collaborate and make it happen!</p>
           <Box className='contact-info'>
             <p><strong>Email:</strong> <a href="mailto:B.Asanovic05@gmail.com">B.Asanovic05@gmail.com</a></p>
             <p><strong>Phone:</strong> <a href="tel:+31680109222">+31680109222</a></p>
