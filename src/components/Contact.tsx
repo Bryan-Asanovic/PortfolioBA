61e1e3bed71e9d1abbda997b23df1b5880a45860
Bryan <0346194@student.rocvantwente.nl>
2024-12-16 14:16:53 +0100

    update LinkedIn profile link in Contact component

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -11,7 +11,7 @@ function Contact() {
           <Box className='contact-info'>
             <p><strong>Email:</strong> <a href="mailto:B.Asanovic05@gmail.com">B.Asanovic05@gmail.com</a></p>
             <p><strong>Phone:</strong> <a href="tel:+31680109222">+31680109222</a></p>
-            <p><strong>LinkedIn:</strong> <a href="https://www.linkedin.com/in/yourprofile" target="_blank" rel="noopener noreferrer">linkedin.com/in/yourprofile</a></p>
+            <p><strong>LinkedIn:</strong> <a href="https://www.linkedin.com/in/bryan-asanovic-418463246/" target="_blank" rel="noopener noreferrer">linkedin.com/in/bryan-asanovic</a></p>
             <p><strong>GitHub:</strong> <a href="https://github.com/Bryan-Asanovic" target="_blank" rel="noopener noreferrer">github.com/Bryan-Asanovic</a></p>
           </Box>
         </div>
